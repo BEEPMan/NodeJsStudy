2e9bce0f26d57dea06adb62134e9c972c8914ecb
beepman <bio8641@naver.com>
2019-12-04 16:42:24 +0900

    the end.

--- main.js
+++ main.js
@@ -1,3 +1,44 @@
+var express = require('express');
+var app = express();
+var fs = require('fs');
+var template = require('./lib/template.js');
+var sanitizeHtml = require('sanitize-html');
+var path = require('path');
+var qs = require('querystring');
+var bodyParser = require('body-parser');
+var compression = require('compression');
+var topicRouter = require('./routes/topic.js');
+var indexRouter = require('./routes/index.js');
+var helmet = require('helmet')
+app.use(helmet());
+
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(compression());
+app.get('*',function(request, response, next){
+    fs.readdir('./data', function(error, filelist) {
+        request.list = filelist;
+        next();
+    });
+});
+
+app.use('/',indexRouter);
+app.use('/topic',topicRouter);
+
+app.use(function(req, res, next){
+    res.status(404).send('Sorry cant find that!');
+});
+
+app.use(function(err, req, res, next){
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+
+/*
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
@@ -150,3 +191,4 @@ var app = http.createServer(function(request,response){
     
 });
 app.listen(3000);
+*/
